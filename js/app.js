0e4884ea31c1b8801e336c90190e63e12962c239
tsimeonov <tsimeonov12@gmail.com>
2024-03-23 16:58:13 +0100

    when one btn clicked remove the other two

--- js/app.js
+++ js/app.js
@@ -9,14 +9,20 @@ let searchBtn = document.querySelector("#search-btn");
 
 menuBtn.addEventListener("click", () => {
   navbar.classList.toggle("active");
+  searchForm.classList.remove("active");
+  cartItem.classList.remove("active");
 });
 
 cartBtn.addEventListener("click", () => {
   cartItem.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchForm.classList.remove("active");
 });
 
 searchBtn.addEventListener("click", () => {
   searchForm.classList.toggle("active");
+  navbar.classList.remove("active");
+  cartItem.classList.remove("active");
 });
 
 window.onscroll = () => {
