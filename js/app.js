c060b53ebcd68b70f653e357eb0fcffdae77e2b5
tsimeonov <tsimeonov12@gmail.com>
2024-03-23 16:05:39 +0100

    add the functionality to the search btn

--- js/app.js
+++ js/app.js
@@ -4,10 +4,17 @@ let menuBtn = document.querySelector("#menu-btn");
 let cartItem = document.querySelector(".cart-items-container");
 let cartBtn = document.querySelector("#cart-btn");
 
+let searchForm = document.querySelector(".search-form");
+let searchBtn = document.querySelector("#search-btn");
+
 menuBtn.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
 
 cartBtn.addEventListener("click", () => {
   cartItem.classList.toggle("active");
+});
+
+searchBtn.addEventListener("click", () => {
+  searchForm.classList.toggle("active");
 });
