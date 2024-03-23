e42bbad56f6a81e75844662348c1df3ef5da5c72
tsimeonov <tsimeonov12@gmail.com>
2024-03-23 16:01:21 +0100

    add the functionality for the cart

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 let navbar = document.querySelector(".navbar");
 let menuBtn = document.querySelector("#menu-btn");
 
+let cartItem = document.querySelector(".cart-items-container");
+let cartBtn = document.querySelector("#cart-btn");
+
 menuBtn.addEventListener("click", () => {
   navbar.classList.toggle("active");
+});
+
+cartBtn.addEventListener("click", () => {
+  cartItem.classList.toggle("active");
 });
